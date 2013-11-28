ee9e26dc629fb32434be75c20b6648d266ec962e
Fergus <fergus@users.noreply.github.com>
2013-11-29 10:36:01 +1100

    Add on_activated_async event changed other events to async

--- ShowEncoding.py
+++ ShowEncoding.py
@@ -1,13 +1,17 @@
 # coding=utf8
+# Added updating on view activation to capture changes on application reload with open tabs (FS 20131129)
+# Moved event handlers for on_load and on_post_save to no blocking async calls (FS 20131129)
 import sublime, sublime_plugin
 
 class EncodingPlugin(sublime_plugin.EventListener):
     def __init__(self):
         return
-    def on_load(self, view):
+    def on_load_async(self, view):
         self.update_encoding(view)
-    def on_post_save(self, view):
+    def on_post_save_async(self, view):
     	self.update_encoding(view)
+    def on_activated_async(self, view)
+        self.update_encoding(view)
 
     def update_encoding(self, view):
     	view.set_status('ShowEncodingPluginStatus', view.encoding() if view.encoding() != u'Undefined' else sublime.load_settings('Preferences.sublime-settings').get('default_encoding', 'Unknown Encoding'))
